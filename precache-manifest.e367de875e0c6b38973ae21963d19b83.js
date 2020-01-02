ed6831ebce92d8cb6158f334c08472f768ad5a50
Havir-S <Havir-S@users.noreply.github.com>
2020-01-02 12:50:16 +0100

    Updates

---
+++ precache-manifest.e367de875e0c6b38973ae21963d19b83.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c20dd53b26fcaab088d7310bc1936d31",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "e48d41bb712eb80a2157",
+    "url": "/weather-app/static/css/main.313ef5c5.chunk.css"
+  },
+  {
+    "revision": "b2313e5fc14b964a07b4",
+    "url": "/weather-app/static/js/2.f531efd9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather-app/static/js/2.f531efd9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e48d41bb712eb80a2157",
+    "url": "/weather-app/static/js/main.9bba2b9e.chunk.js"
+  },
+  {
+    "revision": "df1ac26dc0e7b7d18b20",
+    "url": "/weather-app/static/js/runtime-main.a7aa11b4.js"
+  },
+  {
+    "revision": "f2418a2d65826e8648f1a004ee5fe81b",
+    "url": "/weather-app/static/media/clouds.f2418a2d.svg"
+  },
+  {
+    "revision": "78ff478294b58680f12358927d489b80",
+    "url": "/weather-app/static/media/dawn.78ff4782.svg"
+  },
+  {
+    "revision": "dca05a71c1c16dae1558b115dcf2e1db",
+    "url": "/weather-app/static/media/globe.dca05a71.svg"
+  },
+  {
+    "revision": "20d9a18beca7311bd6842028df76de3d",
+    "url": "/weather-app/static/media/humidity.20d9a18b.svg"
+  },
+  {
+    "revision": "9232630a4fd4fdc08180bc65751c2149",
+    "url": "/weather-app/static/media/manometer.9232630a.svg"
+  },
+  {
+    "revision": "b2eb77904ec9560113e07e08b97beb6a",
+    "url": "/weather-app/static/media/sunset.b2eb7790.svg"
+  },
+  {
+    "revision": "cbebe7b52a584d7d634ecb2e40552269",
+    "url": "/weather-app/static/media/television.cbebe7b5.svg"
+  },
+  {
+    "revision": "f592f9a37c68ad4f71397030560c8932",
+    "url": "/weather-app/static/media/thermometer.f592f9a3.svg"
+  },
+  {
+    "revision": "5d97d9c6f058deea1a11f355c8735aaf",
+    "url": "/weather-app/static/media/windy.5d97d9c6.svg"
+  }
+]);
